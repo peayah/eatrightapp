3b15bb22e4986de81854d86dbcf6a4466c314264
peayah <82358003+peayah@users.noreply.github.com>
2021-11-01 00:20:27 -0700

    set up food model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Food(models.Model):
+    """Model representing a food item"""
+    name = models.CharField(max_length=200,
+                            help_text='Enter Food Item')
+
+    calories = models.IntegerField('Amount of calories', null=True, blank=True)
+
+    potasium = models.IntegerField('Amount of potasium', null=True, blank=True)
+
+    KIND_OF_FOOD = (
+        ('b', 'Bread'),
+        ('f', 'Fruit'),
+        ('m', 'Meat'),
+        ('g', 'Greens'),
+        ('w', 'Water'),
+        ('c', 'Caffine'),
+    )
+
+    kind = models.CharField(
+        max_length=1,
+        choices=KIND_OF_FOOD,
+        blank=True,
+        default='m',
+        help_text='Kind of food',
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('food-detail', args=[str(self.id)])

b57dee8b4aa921dd05c27e6d50ebacb56a7e6252
peayah <82358003+peayah@users.noreply.github.com>
2023-02-18 01:20:43 -0800

    add allowed host heroku

--- eatright/settings.py
+++ eatright/settings.py
@@ -27,10 +27,10 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://desolate-brook-11058.herokuapp.com',]
 
 
 # Application definition

e08ba95b6f9faca77e9e1bfa7ade2fe89a1c1230
peayah <82358003+peayah@users.noreply.github.com>
2023-02-18 15:19:17 -0800

    update settings and requiremnts

--- eatright/settings.py
+++ eatright/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'eatright.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eatrightdb',
+        'USER': 'postres',
+        'PASSWORD': 'FOOD610ba?',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
